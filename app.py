bfff92dc3e4bfd66f06034bf8d73a588da139aab
suelensantos <suelensantos.cordeiro@gmail.com>
2024-01-29 00:05:58 -0300

    feat: add login and logout routes for the authentication process

--- app.py
+++ app.py
@@ -2,18 +2,31 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy # ORM - objeto de relação mapeador - gera uma camada de abstração para o banco de dados (fica acima do banco).
 # Código interage com o ORM (onde será feita todas as alterações), que interage com o banco de dados
 from flask_cors import CORS
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
+# UserMixin é utilizado para fazer o login do usuário
+# LoginManager faz o gerenciamento de usuário, quem está logado ou não
+# login_required permite não habilitar o acesso de usuários não logados às rotas (específicas), ou seja, ela obriga o usuário a estar autenticado nas rotas específicas
 
 app = Flask(__name__)
-# precisa configurar o caminho do banco (banco de arquivos)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+app.config['SECRET_KEY'] = 'minha_chave_123' # necessário para fazer o login do usuário a partir da lib LoginManager
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db' # precisa configurar o caminho do banco (banco de arquivos)
 
-# iniciar a conexão com o banco de dados (BD)
-db = SQLAlchemy(app)
+login_manager = LoginManager()
+db = SQLAlchemy(app) # iniciar a conexão com o banco de dados (BD)
+login_manager.init_app(app)
+login_manager.login_view = 'login' # habilita a autenticação do usuário no login, criando uma sessão (Set-Cookie) no header da requisição (caso limpe o cookie, o usuário perderá o acesso do login, tendo que logar novamente)
 CORS(app)
 
 # Criar o model das tabelas do banco (produto)
 # Para transformar o model em tabela no banco, basta digitar o comando 'flask shell' e, após, digitar '>>> db.create_all()'.
+# '>>> db.drop_all()' -> serve para limpar todas as tabelas do banco
+# '>>> db.create_all()'
+# '>>> db.session.commit()'
 # Com o comando '>>> db.session.commit()', temos a *session*, que é a propriedade do BD que armazena a **conexão** com o banco. Já o *commit*, envia para o banco e efetiva as mudanças feitas.
+class User(db.Model, UserMixin):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(80), nullable=False, unique=True)
+  password = db.Column(db.String(80), nullable=False)
 
 class Product(db.Model):
   # colunas
@@ -22,7 +35,31 @@ class Product(db.Model):
   price = db.Column(db.Float, nullable=False)
   description = db.Column(db.Text, nullable=True)
 
+# Autenticação - essa função existe porque toda vez que eu fizer uma requisição em uma rota protegida, o @login_required vai precisar recuperar o usuário que está
+# tentando acessar essa rota.
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(int(user_id))
+
+@app.route('/login', methods=["POST"])
+def login():
+  data = request.json
+  user = User.query.filter_by(username=data.get("username")).first()
+
+  if user and data.get("password") == user.password:
+    login_user(user)
+    return jsonify({'message': "Logged in sucessfully!"})
+
+  return jsonify({'message': "Unauthorized! Invalid credentials."}), 401
+
+@app.route('/logout', methods=["POST"])
+@login_required
+def logout():
+  logout_user()
+  return jsonify({'message': "Logout sucessfully!"})
+
 @app.route('/api/products/add', methods=["POST"])
+@login_required # protege o acesso do usuário à rota (acesso liberado só quando estiver logado)
 def add_products():
   data = request.json
   if 'name' in data and 'price' in data:
@@ -33,6 +70,7 @@ def add_products():
   return jsonify({'message': "Invalid product data!"}), 400
 
 @app.route('/api/products/delete/<int:product_id>', methods=["DELETE"])
+@login_required
 def delete_product(product_id):
   # Recuperar o produto da base de dados
   # Verificar se o produto existe
@@ -57,6 +95,7 @@ def get_product_details(product_id):
   return jsonify({ 'message': "Product not found!"}), 404
 
 @app.route('/api/products/update/<int:product_id>', methods=["PUT"])
+@login_required
 def update_product(product_id):
   product = Product.query.get(product_id)
   if not product:
